9c0cf54ed0527c869dd65fbddc0e63afb52244ea
aslia <asli.r.asilturk@gmail.com>
2025-05-01 00:58:22 -0500

    Adding comments: libraries, random color function, displayable hex code function

--- MyPalette.py
+++ MyPalette.py
@@ -1,1 +1,11 @@
 #start here
+
+#import libraries needed
+#for sure add PIL, random
+
+#define random color function
+#return
+
+#define displayable hex code function
+#converts last output into proper hex code
+#return
